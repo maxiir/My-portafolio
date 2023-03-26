7bf0d71766045cf75723b75b503a470bfbb3804a
Maxi <maxiirucci@gmail.com>
2023-03-26 20:58:51 -0300

    new deploy

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
 export const sendData = async (values) => 
-    await axios.post('https://my-portafolio-beryl.vercel.app/api/sendemail', values) //modificar dependiendo el proveedor de deploy
+    await axios.post('https://my-portafolio-production.up.railway.app/api/sendemail', values) //modificar dependiendo el proveedor de deploy
