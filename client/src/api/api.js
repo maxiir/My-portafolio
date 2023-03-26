60214fb9906f11261202421bd3b8f149261cfe57
Maxi <maxiirucci@gmail.com>
2023-03-26 19:19:06 -0300

    fix email

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-export const sendData = async (values) => {
-    await axios.post('https://my-portafolio-hm8z.vercel.app/api/sendemail', values)
+export const sendData = (values) => {
+    axios.post('https://my-portafolio-hm8z.vercel.app/api/sendemail', values)
 }
