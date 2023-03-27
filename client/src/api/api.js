c30d3a5b1609db03ee830cf1014a0161b0a52773
Maxi <maxiirucci@gmail.com>
2023-03-26 23:51:41 -0300

    test vercel

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
 export const sendData = async (values) => 
-    await axios.post('https://my-portafolio-beryl.vercel.app/api/sendemail', values) //modificar dependiendo el proveedor de deploy
+    await axios.post('https://my-portafolio-maxiir.vercel.app/api/sendemail', values) //modificar dependiendo el proveedor de deploy
