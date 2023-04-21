356e1f080b7f485e50a7066948351556ef42cfa7
Maxi <maxiirucci@gmail.com>
2023-04-21 16:49:41 -0300

    fix response

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
         </svg>
       </div>
       <Skills />
-      <div className="wave">
+      <div className="wave-bajo">
 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path
@@ -33,6 +33,7 @@ function Home() {
             d="M0,128L80,138.7C160,149,320,171,480,160C640,149,800,107,960,106.7C1120,107,1280,149,1360,170.7L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
             ></path>
         </svg>
+
       </div>
       <Proyects />
       <Contact />
