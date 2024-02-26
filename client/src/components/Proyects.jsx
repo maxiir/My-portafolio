e2cae6a2b460f8c29b2455af8dc29dda2bd859b0
Maxi <maxiirucci@gmail.com>
2024-02-26 16:00:54 -0300

    new changes in portfolio

--- client/src/components/Proyects.jsx
+++ client/src/components/Proyects.jsx
@@ -9,40 +9,39 @@ function Proyects() {
         <div className="proyects">
           <div className="efect">
             <div className="proyect">
-              <h5>Estudio contable web site</h5>
+              <h5>Sistema ecommerce para emprendimiento</h5>
+              <img src="/proyecto-ecommerce.png" alt="proyect1" />
+              <p>
+                El sitio web ecommerce, fue desarrollado con la finalidad de que el emprendimiento tenga un mayor numero de ventas en linea y fue desarrollado con react y nodejs garantizando una solucion escalable y atractiva para los usuarios 
+              </p>
+              <a className="btn btn-primary" href="https://www.nordicasocks.shop/">
+                Ver
+              </a>
+            </div>
+          </div>
+
+          <div className="efect">
+            <div className="proyect">
+              <h5>Single page para estudio contable</h5>
               <img src="/proyecto-contable.png" alt="proyect1" />
-              <p>
-                El sitio web contable, fue desarrollado con la finalidad de que el cliente tenga una mejor presensia en linea y fue desarrollado con react y nodejs garantizando una solucion escalable y atractiva para los usuarios 
-              </p>
-              <a className="btn btn-primary" href="#">
-                Ver
-              </a>
-            </div>
-          </div>
-
-          <div className="efect">
-            <div className="proyect">
-              <h5>Web movies</h5>
-              <img src="/web-movies.png" alt="proyect1" />
-              <p>
-                La aplicación fue creada utilizando la tecnología React y Nodejs,
-                brindando una experiencia de usuario atractiva y eficiente.
-              </p>
-              <a className="btn btn-primary" href="#">
-                Ver
-              </a>
-            </div>
-          </div>
-
-          <div className="efect">
-            <div className="proyect">
-              <h5>Estudio juridico web site</h5>
-              <img src="/proyecto-juridico.png" alt="proyect1" />
               <p>
                 Desarrolle un sitio web para un estudio jurídico con el objetivo
                 de ampliar su alcance y brindar una presencia en línea.
               </p>
-              <a className="btn btn-primary" href="https://estudiojuridico-tw.web.app/">
+              <a className="btn btn-primary" href="https://estudiocontablecontable.vercel.app/">
+                Ver
+              </a>
+            </div>
+          </div>
+
+          <div className="efect">
+            <div className="proyect">
+              <h5>Proyecto WordPress</h5>
+              <img src="/proyecto-wordpress.png" alt="proyect1" />
+              <p>
+                Actualice y mejore el sitio web utilizando WordPress. Implementando nuevas funcionalidades y características, mejorando su accesibilidad. Además, trabajé en la actualización del diseño y la estructura del contenido para reflejar de manera más efectiva la identidad de la institución y cumplir con los objetivos establecidos.
+              </p>
+              <a className="btn btn-primary" href="https://www.sectw.com.ar/">
                 Ver
               </a>
             </div>
